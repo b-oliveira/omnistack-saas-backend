f9834e81c59369aace09bacf29a52caf24d654e5
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-02 19:33:51 -0300

    Configures rules and permissions

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -2,6 +2,9 @@ const Env = use('Env');
 
 const User = use('App/Models/User');
 
+const Role = use('Adonis/Acl/Role');
+const Permission = use('Adonis/Acl/Permission');
+
 class DatabaseSeeder {
   async run() {
     const user = await User.create({
@@ -10,10 +13,45 @@ class DatabaseSeeder {
       password: Env.get('SEED_USER_PASSWORD'),
     });
 
-    await user.teams().create({
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar membros',
+    });
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Criar projetos',
+    });
+
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrador',
+    });
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderador',
+    });
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitante',
+    });
+
+    await admin.permissions().attach([createInvite.id, createProject.id]);
+    await moderator.permissions().attach([createProject.id]);
+
+    const team = await user.teams().create({
       name: Env.get('SEED_TEAM_NAME'),
       user_id: user.id,
     });
+
+    const teamJoin = await user
+      .teamJoins()
+      .where('team_id', team.id)
+      .first();
+
+    await teamJoin.roles().attach([admin.id]);
   }
 }
 
