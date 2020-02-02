c883200c492bfbacea2f62d2f240a490997e05a7
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-02 11:11:45 -0300

    Project CRUD

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,3 +1,50 @@
-class ProjectController {}
+class ProjectController {
+  async index({ request }) {
+    const projects = request.team.projects().fetch();
+
+    return projects;
+  }
+
+  async store({ request }) {
+    const data = request.only(['title']);
+
+    const project = request.team.projects().create(data);
+
+    return project;
+  }
+
+  async show({ params, request }) {
+    const project = await request.team
+      .projects()
+      .where('id', params.id)
+      .first();
+
+    return project;
+  }
+
+  async update({ params, request }) {
+    const project = await request.team
+      .projects()
+      .where('id', params.id)
+      .first();
+
+    const data = request.only(['title']);
+
+    project.merge(data);
+
+    await project.save();
+
+    return project;
+  }
+
+  async destroy({ params, request }) {
+    const project = await request.team
+      .projects()
+      .where('id', params.id)
+      .first();
+
+    await project.delete();
+  }
+}
 
 module.exports = ProjectController;
